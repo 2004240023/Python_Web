1421c48c3cb2dc37b059636665eb8324d2012f7c
2004240023 <2004240023yr@edu.oca.ac.jp>
2022-07-02 17:51:40 +0900

    commit

--- app.py
+++ app.py
@@ -75,8 +75,113 @@ def human_search():
 def human_result():
     search_height = request.args.get("search_height")
     search_weight = request.args.get("search_weight")
-    humans = db.session.query(Human).filter(or_(Human.height >= search_height,Human.weight >= search_weight))
-    return render_template('./human_result.html', humans=humans, search_height=search_height,search_weight=search_weight)
+    select1 = request.args.get("select1")
+    select2 = request.args.get("select2")
+    radio = request.args.get("radio")
+
+    humans_8 = db.session.query(Human).filter(Human.height <= search_height, Human.weight >= search_weight)#身長以下and体重以上
+    humans_7 = db.session.query(Human).filter(Human.height >= search_height, Human.weight <= search_weight)#身長以上and体重以下
+    humans_6 = db.session.query(Human).filter(Human.height <= search_height, Human.weight <= search_weight)#身長以下and体重以下
+    humans_5 = db.session.query(Human).filter(Human.height >= search_height, Human.weight >= search_weight)#身長以上and体重以上
+
+    humans_4 = db.session.query(Human).filter(or_(Human.height <= search_height, Human.weight >= search_weight))#身長以下or体重以上
+    humans_3 = db.session.query(Human).filter(or_(Human.height >= search_height, Human.weight <= search_weight))#身長以上or体重以下
+    humans_2 = db.session.query(Human).filter(or_(Human.height <= search_height, Human.weight <= search_weight))#身長以下or体重以下
+    humans_1 = db.session.query(Human).filter(or_(Human.height >= search_height, Human.weight >= search_weight))#身長以上or体重以上
+    return render_template('./human_result.html', humans=humans_1, search_height=search_height,search_weight=search_weight,select1=select1, select2=select2, radio=radio )
+
+
+#
+
+# ここからWebアプリ２の演習の回答例
+
+
+@app.route('/human_search')
+def human_search():
+    return render_template('./human_search.html')
+
+
+@app.route('/human_result')
+def human_result():
+    search_height = request.args.get('search_height')
+    search_weight = request.args.get('search_weight')
+    humans = db.session.query(Human).filter(
+        or_(Human.height >= search_height, Human.weight >= search_weight))
+    return render_template(
+        './human_result.html',
+        humans=humans,
+        search_height=search_height,
+        search_weight=search_weight)
+
+# ここからWebアプリ3の演習の回答例
+
+
+# @app.route('/human_search2')
+# def human_search2():
+#     return render_template('./human_search2.html')
+
+
+# @app.route('/human_result2')
+# def human_result2():
+#     search_height = float(request.args.get('search_height'))
+#     search_weight = float(request.args.get('search_weight'))
+#     height_cond = int(request.args.get('height_cond'))
+#     weight_cond = int(request.args.get('weight_cond'))
+
+#     def cond_fn(a, b, cond):
+#         print(f'type:{type(cond)}', file=sys.stdout)
+#         if cond == 1:
+#             return a >= b
+#         else:
+#             return a <= b
+
+#     def height_cond_fn():
+#         return cond_fn(Human.height, search_height, height_cond)
+
+#     def weight_cond_fn():
+#         return cond_fn(Human.weight, search_weight, weight_cond)
+
+#     and_or = int(request.args.get('and_or'))
+#     print(f'and_or:{and_or}', file=sys.stdout)
+
+#     def and_or_fn(a, b):
+#         if and_or == 1:
+#             return and_(a, b)
+#         else:
+#             return or_(a, b)
+
+#     humans = db.session.query(Human).filter(
+#         and_or_fn(height_cond_fn(), weight_cond_fn()))
+    
+#     def make_list(name, as_str=False):
+#         def _convert(param):
+#             if as_str:
+#                 result = f"'{param}'"
+#             else:
+#                 result = param
+#             return result
+
+#         return ','.join([_convert(str(getattr(human, name))) for human in humans])
+
+#     x_list = make_list('height')
+#     y_list = make_list('weight')
+#     name_list = make_list('name', as_str=True)
+
+#     return render_template(
+#         './human_result2.html',
+#         humans=humans,
+#         search_height=search_height,
+#         search_weight=search_weight,
+#         height_cond=height_cond,
+#         weight_cond=weight_cond,
+#         and_or=and_or,
+#         x_list=x_list,
+#         y_list=y_list,
+#         name_list=name_list)
+
+
+#
+
 
 @app.route('/try_html')
 def try_html():
